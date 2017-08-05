18219209b2f37fbdbe98325fa8bef52b4f2a787f
Alexander Kerner <alexanderkerner24@gmail.com>
2017-08-05 12:04:58 +0200

    Initial commit

---
+++ src/main/java/com/github/ktools1000/RandomString.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Alexander Kerner. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.github.ktools1000;
+
+import java.util.Random;
+
+public class RandomString {
+
+	public static final char[] DEFAULT_CHARS = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+	public static final int DEFAULT_LENGTH = 6;
+
+	private char[] chars;
+
+	private int length;
+
+	public RandomString() {
+		this.chars = DEFAULT_CHARS;
+		this.length = DEFAULT_LENGTH;
+	}
+
+	char[] getChars() {
+		return chars;
+	}
+
+	int getLength() {
+		return length;
+	}
+
+	public RandomString ofLength(final int lenth) {
+		return setLength(lenth);
+	}
+
+	RandomString setChars(final char[] chars) {
+		this.chars = chars;
+		return this;
+	}
+
+	RandomString setLength(final int length) {
+		this.length = length;
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		final Random random = new Random();
+		for (int i = 0; i < getLength(); i++) {
+			final char c = getChars()[random.nextInt(getChars().length)];
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+
+	public RandomString withCharacters(final char[] chars) {
+		return setChars(chars);
+	}
+}
---
+++ src/main/java/com/github/ktools1000/io/BackupCreator.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Alexander Kerner. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.github.ktools1000.io;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BackupCreator {
+
+	private static final Logger logger = LoggerFactory.getLogger(BackupCreator.class);
+
+	public void makeBackup(final File file) {
+		if (file.exists() && file.length() != 0) {
+			try {
+				final File backupFile = new File(file.getParentFile(), file.getName() + ".bak");
+				FileUtils.copyFile(file, backupFile);
+				if (logger.isDebugEnabled()) {
+					logger.debug("Backup created as " + backupFile);
+				}
+			} catch (final IOException e) {
+				if (logger.isErrorEnabled()) {
+					logger.error(e.getLocalizedMessage(), e);
+				}
+			}
+		}
+	}
+}
