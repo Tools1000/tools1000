d87f1d29fd27f1555f5b9c3755888afdd94e6ad4
Alexander Kerner <alexkerner1000@gmail.com>
2023-02-27 08:34:17 +0100

    Use nio to copy files

--- src/main/java/com/github/tools1000/io/BackupCreator.java
+++ src/main/java/com/github/tools1000/io/BackupCreator.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2022 kerner1000. All rights reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,13 +15,15 @@
  ******************************************************************************/
 package com.github.tools1000.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
 
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 /**
  * Provides static methods to create a backup-copy of a file and restore a file
@@ -44,13 +46,14 @@ public class BackupCreator {
     public File makeBackup(final File file) throws FileNotFoundException {
 	if (file.exists()) {
 	    try {
-		final File backupFile = new File(file.getParentFile(), file.getName() + ".bak");
-		FileUtils.copyFile(file, backupFile);
-		if (logger.isDebugEnabled()) {
-		    logger.debug("Backup created as " + backupFile);
-		}
-		return backupFile;
-	    } catch (final IOException e) {
+			final File backupFile = new File(file.getParentFile(), file.getName() + ".bak");
+			Files.copy(file.toPath(), backupFile.toPath(), REPLACE_EXISTING);
+	//		FileUtils.copyFile(file, backupFile);
+			if (logger.isDebugEnabled()) {
+				logger.debug("Backup created as " + backupFile);
+			}
+			return backupFile;
+		} catch (final IOException e) {
 		if (logger.isErrorEnabled()) {
 		    logger.error(e.getLocalizedMessage(), e);
 		}
@@ -70,19 +73,20 @@ public class BackupCreator {
     public File restoreBackup(final File backupFile) throws FileNotFoundException {
 	if (backupFile.canRead() && backupFile.getName().endsWith(".bak")) {
 	    try {
-		final String oldFileName = backupFile.getName().substring(0, backupFile.getName().length() - 4);
-		final File file = new File(backupFile.getParentFile(), oldFileName);
-		FileUtils.copyFile(backupFile, file);
-		if (logger.isDebugEnabled()) {
-		    logger.debug("Backup restored to " + file);
-		}
-		if(!backupFile.delete()){
-			if(logger.isWarnEnabled()){
-				logger.warn("Failed to delete backup file");
+			final String oldFileName = backupFile.getName().substring(0, backupFile.getName().length() - 4);
+			final File file = new File(backupFile.getParentFile(), oldFileName);
+	//		FileUtils.copyFile(backupFile, file);
+			Files.copy(backupFile.toPath(), file.toPath(), REPLACE_EXISTING);
+			if (logger.isDebugEnabled()) {
+				logger.debug("Backup restored to " + file);
 			}
-		}
-		return file;
-	    } catch (final IOException e) {
+			if (!backupFile.delete()) {
+				if (logger.isWarnEnabled()) {
+					logger.warn("Failed to delete backup file");
+				}
+			}
+			return file;
+		} catch (final IOException e) {
 		if (logger.isErrorEnabled()) {
 		    logger.error(e.getLocalizedMessage(), e);
 		}
